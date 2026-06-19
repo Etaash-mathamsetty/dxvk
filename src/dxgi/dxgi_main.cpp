d5f236b6fd9d50ac548101bb6552a1e25ddf8d35
Etaash Mathamsetty <45927311+Etaash-mathamsetty@users.noreply.github.com>
2026-06-19 17:52:17 -0400

    [dxgi] avoid a warn message

--- src/dxgi/dxgi_main.cpp
+++ src/dxgi/dxgi_main.cpp
@@ -23,7 +23,7 @@ namespace dxvk {
 
 extern "C" {
   DLLEXPORT HRESULT __stdcall CreateDXGIFactory2(UINT Flags, REFIID riid, void **ppFactory) {
-    dxvk::Logger::warn("CreateDXGIFactory2: Ignoring flags");
+    if (Flags) dxvk::Logger::warn("CreateDXGIFactory2: Ignoring flags");
     return dxvk::createDxgiFactory(Flags, riid, ppFactory);
   }
 
